49311da98eb88471ac12400ac226a80823b780d4
SJ70 <50670730+SJ70@users.noreply.github.com>
2023-05-12 04:08:00 +0900

    feat: 시뮬레이터 목록을 위한 'Problem' 클래스 작성

---
+++ src/class/Problem.tsx
@@ -0,0 +1,46 @@
+class Problem{
+    private code: number;
+    private level: number;
+    private title: string;
+    private desc: string;
+    private tag: string[];
+    private simulator: JSX.Element;
+
+    constructor(code:number, level:number, title:string, desc:string, tag:string[], simulator:JSX.Element){
+        this.code = code;
+        this.level = level;
+        this.title = title;
+        this.desc = desc;
+        this.tag = tag;
+        this.simulator = simulator;
+    }
+
+    public getCode(): number{
+        return this.code;
+    }
+    public getLevel(): number{
+        return this.level;
+    }
+    public getTitle(): string{
+        return this.title;
+    }
+    public getDesc(): string {
+        return this.desc;
+    }
+    public getTag(): string[]{
+        return this.tag;
+    }
+    public getComp(): JSX.Element{
+        return this.simulator;
+    }
+    public getSrc(): string{
+        return "https://www.acmicpc.net/problem/"+this.code;
+    }
+
+    public hasTag(str:string): boolean{
+        return this.tag.includes(str);
+    }
+
+}
+
+export default Problem;
