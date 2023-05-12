2eadfec1fe5b33301ec83a0abaf51e7532497c7f
SJ70 <50670730+SJ70@users.noreply.github.com>
2023-05-13 01:25:18 +0900

    Design(N_Queen): 퍼블리싱

--- src/simulator/N_Queen/N_Queen.tsx
+++ src/simulator/N_Queen/N_Queen.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import './N_Queen.css';
 
 const N_Queen = () => {
   return (
-    <div className="">
-      n_queen
+    <div className="N_Queen">
+      <div className="N">
+        <div className="row">
+          <p>N = </p>
+          <input type="text" placeholder='8'></input>
+        </div>
+        <input type="range"></input>
+      </div>
+      <div className="FPS">
+        <div className="row">
+          <p>초당 프레임 수 : </p>
+          <input type="text" placeholder='1'></input>
+        </div>
+        <input type="range"></input>
+      </div>
+      <button>시작하기</button>
     </div>
   );
 }
