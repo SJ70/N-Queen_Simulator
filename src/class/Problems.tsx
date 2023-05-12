9606d05ae97ffce18f286e44178049813833fc9a
SJ70 <50670730+SJ70@users.noreply.github.com>
2023-05-12 16:19:47 +0900

    refactor: enum Tag 모듈화

--- src/class/Problems.tsx
+++ src/class/Problems.tsx
@@ -1,9 +1,7 @@
 import Problem from "./Problem";
-import N_Queen from "../simulator/N_Queen/N_Queen";
+import Tag from "./Tag";
 
-const enum Tag {
-    구현, 시뮬레이션, 정렬, 이분탐색, 그리디, BFS, DFS, DP, 누적합, 백트래킹, 브루트포스
-}
+import N_Queen from "../simulator/N_Queen/N_Queen";
 
 class Problems{
     private Arr: Problem[] = [];
---
+++ src/class/Tag.tsx
@@ -0,0 +1,5 @@
+const enum Tag {
+    구현, 시뮬레이션, 정렬, 이분탐색, 그리디, BFS, DFS, DP, 누적합, 백트래킹, 브루트포스
+}
+
+export default Tag;
