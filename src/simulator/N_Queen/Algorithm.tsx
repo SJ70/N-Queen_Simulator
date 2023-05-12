d5a9f32a12b6c7c206a87f6c4e9761d7782b4f7c
SJ70 <50670730+SJ70@users.noreply.github.com>
2023-05-13 01:25:55 +0900

    feat(N_Queen): 알고리즘 클래스 작성 (소량)

---
+++ src/simulator/N_Queen/Algorithm.tsx
@@ -0,0 +1,19 @@
+class N_Queen{
+    
+    private checkC: boolean[][];
+    private checkDir1: boolean[][];
+    private checkDir2: boolean[][];
+
+    constructor(N: number){
+        this.checkC = new Array(N);
+        this.checkDir1 = new Array(N);
+        this.checkDir2 = new Array(N);
+        for(let i=0; i<N; i++){
+            this.checkC[i] = new Array(N);
+            this.checkDir1[i] = new Array(N);
+            this.checkDir2[i] = new Array(N);
+        }
+    }
+}
+
+export default N_Queen;
