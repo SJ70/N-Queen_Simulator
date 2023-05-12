7544a894dc31cfc2f705d9a85d3523bbf24879fb
SJ70 <50670730+SJ70@users.noreply.github.com>
2023-05-13 01:24:38 +0900

    design(Simulator): 시뮬레이터에서 컴포넌트를 Restrict로 감싸서 return

--- src/pages/Simulator.tsx
+++ src/pages/Simulator.tsx
@@ -1,10 +1,15 @@
 import Problem from '../class/Problem';
 import Problems from '../class/Problems';
 import { useParams } from "react-router-dom";
+import "../components/Restrict.css";
 
 const Simulator = ({Probs}: {Probs:Problems}) => {
     const id: number = Number(useParams().id);
-    return Probs.searchProblem(id).getComp();
+    return(
+        <div className="Restrict">
+            {Probs.searchProblem(id).getComp()}
+        </div>
+    ) 
 }
 
 export default Simulator;
