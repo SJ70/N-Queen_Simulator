5387ccae78fbf2a60a6b8b7c93ff45ccd9c18de8
SJ70 <seojae70@gmail.com>
2023-05-15 15:41:11 +0900

    feat(N_Queen): 알고리즘 작성

--- src/simulator/N_Queen/N_Queen_Logic.tsx
+++ src/simulator/N_Queen/N_Queen_Logic.tsx
@@ -1,18 +1,39 @@
 class N_Queen_Logic{
     
-    private checkC: boolean[][];
-    private checkDir1: boolean[][];
-    private checkDir2: boolean[][];
+    private N;
+    private checkC: boolean[];
+    private checkDir1: boolean[];
+    private checkDir2: boolean[];
+    private result: number = 0;
 
     constructor(N: number){
+        this.N = N;
         this.checkC = new Array(N);
         this.checkDir1 = new Array(N);
         this.checkDir2 = new Array(N);
-        for(let i=0; i<N; i++){
-            this.checkC[i] = new Array(N);
-            this.checkDir1[i] = new Array(N);
-            this.checkDir2[i] = new Array(N);
+    }
+
+    private BT(r:number, c:number): void{
+        if(r==this.N){
+            this.result++;
+            return;
+        } 
+
+        let d1: number = r+c;   // 우상향대각
+        let d2: number = this.N-1-r+c;  // 우하향대각
+
+        if(!this.checkC[c] && !this.checkDir1[d1] && !this.checkDir2[d2]){
+            this.checkC[c] = this.checkDir1[d1] = this.checkDir2[d2] = true;
+            this.BT(r+1, 0);
+            this.checkC[c] = this.checkDir1[d1] = this.checkDir2[d2] = false;
         }
+        if(c+1<this.N){
+            this.BT(r, c+1)
+        }
+    }
+
+    public getResult(): number{
+        return this.result;
     }
 }
 
