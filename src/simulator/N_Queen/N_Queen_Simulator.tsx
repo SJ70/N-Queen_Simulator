25a6fd67fd7b8bec743a1a22d5499f552352e965
SJ70 <seojae70@gmail.com>
2023-05-16 23:28:15 +0900

    feat(N_Queen): 시뮬레이터 페이지 구현 (체스판 보드 표시, 보드의 칸 별 setState())

--- src/simulator/N_Queen/N_Queen_Simulator.tsx
+++ src/simulator/N_Queen/N_Queen_Simulator.tsx
@@ -1,15 +1,78 @@
 import { useParams } from 'react-router-dom';
+import { useState } from 'react';
 import Algorithm from './N_Queen_Algorithm';
+import './N_Queen_Simulator.css';
+import { Decrypt } from './Crypto';
+
+let useStateArr: React.Dispatch<React.SetStateAction<number>>[][];
 
 const N_Queen_Simulator = () => {
+
     const { n, fps } = useParams();
     const N: number = Number(n);
     const FPS: number = Number(fps);
+
     const Algo: Algorithm = new Algorithm(N);
+    const History: number[] = Algo.getHistory();
+
+    document.documentElement.style.setProperty('--N',String(N));
+
+    useStateArr = new Array(N);
+    for(let i=0; i<N; i++){
+        useStateArr[i] = new Array(N);
+    }
+
+    const changeTableValue = (r:number, c:number, value:number) => {
+        useStateArr[r][c](value);
+    }
+
     return (
         <div>
-            시뮬레이터 컴포넌트
+            <Table N={N}/>
+            <button onClick={() => changeTableValue(1,1,1)} > [1][1]의 값을 1로 바꾸어주는 버튼 </button>
         </div>
+    )
+}
+
+const Table = ({N}: {N:number}) => {
+    let arr = []; 
+    for(let r=0; r<N; r++){
+        arr.push(
+            <Row key={r} N={N} row={r}/>
+        );
+    }
+    return (
+        <table>
+            <tbody>
+                {arr}
+            </tbody>
+        </table>
+    )
+}
+const Row = ({N, row}: {N: number, row:number}) => {
+    let arr = [];
+    for(let c=0; c<N; c++){
+        arr.push(
+            <Column key={c} row={row} column={c}/>
+        );
+    }
+    return (
+        <tr>
+            {arr}
+        </tr>
+    )
+}
+const Column = ({row, column}: {row:number, column:number}) => {
+    const [value, setValue] = useState<number>(0);
+    useStateArr[row][column] = setValue;
+
+    const style: string = ((row+column)%2==0) ? "black" : "white";
+    const element: JSX.Element = (value==1) ? (<img src={require("../../img/LightQueen.webp")}></img>) : (<div></div>);
+    return (
+        <td className={style}>
+            {/* {value} */}
+            {element}
+        </td>
     )
 }
 
