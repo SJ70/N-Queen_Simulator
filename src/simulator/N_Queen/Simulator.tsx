5b90472f026904e3fac7848a2354e0f27ab88cdc
SJ70 <50670730+SJ70@users.noreply.github.com>
2023-05-23 03:41:30 +0900

    refactor(N_Queen/Simulator): useState로 동기화할 때마다 algorithm을 재실행하던 현상을 수정, 부가적으로 일부 버그 해결

--- src/simulator/N_Queen/Simulator.tsx
+++ src/simulator/N_Queen/Simulator.tsx
@@ -1,103 +1,133 @@
 import { useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Algorithm from './Algorithm';
 import './Simulator.css';
 import { Decrypt } from './Crypto';
 import { VscChevronLeft, VscChevronRight, VscDebugPause, VscDebugStart } from "react-icons/vsc"
 
 const fps_min: number = 1;
-const fps_max: number = 144;
+const fps_max: number = 1000;
 
-let _useState: React.Dispatch<React.SetStateAction<number>>[][];
-let _getValue: Function[][];
+let N: number;
+let FPS: number;
+let ALGO: Algorithm;
+let HISTORY: number[];
+let USESTATE: React.Dispatch<React.SetStateAction<number>>[][];
+let GETVALUE: Function[][];
+let INDEX: number;
 
-const N_Queen_Simulator = () => {
+let AUTO: NodeJS.Timer;
+let AUTO_FUNC: Function; 
+let AUTO_TOGGLE: boolean;
 
-    const { n } = useParams();
-    const N: number = Number(n);
+const changeIndex = (value:number): void => {
+    let nextIndex = INDEX + value;
+    if( nextIndex > HISTORY.length || nextIndex < 0 ) return;
 
-    const Algo: Algorithm = new Algorithm(N);
-    const History: number[] = Algo.getHistory();
+    let pos: number[] = Decrypt(HISTORY[ (value>0) ? INDEX : INDEX-1 ]);
+    changeValue(pos[0], pos[1]);
+
+    INDEX = nextIndex;
+}
+
+const changeValue = (r:number, c:number): void => {
+    let value = GETVALUE[r][c]();
+    value += value>=100 ? -100 : 100;
+    USESTATE[r][c](value);
+}
+
+const Simulator = () => {
+
+    N = Number(useParams().n);
+    FPS = 1;
+    ALGO = new Algorithm(N);
+    HISTORY = ALGO.getHistory();
+    INDEX = 0;
+    AUTO_TOGGLE = false;
 
     document.documentElement.style.setProperty('--N',String(N));
 
-    _useState = new Array(N);
-    _getValue = new Array(N);
+    USESTATE = new Array(N);
+    GETVALUE = new Array(N);
     for(let i=0; i<N; i++){
-        _useState[i] = new Array(N);
-        _getValue[i] = new Array(N);
-    }
-    const changeTableValue = (r:number, c:number, value:number) => {
-        _useState[r][c](value);
-    }
-
-
-    const [index,setIndex] = useState(0);
-    const changeIndex = (value:number): void => {
-        let i: number = index + value;
-        if(value>0){
-            if(i>History.length){
-                setAuto(false);
-                return;
-            } 
-            let pos: number[] = Decrypt(History[index]);
-            setQueen(pos[0], pos[1]);
-        }
-        else{
-            if(i<0) return;
-            let pos: number[] = Decrypt(History[index-1]);
-            setQueen(pos[0], pos[1]);
-        }
-        setIndex(i);
-    }
-    const setQueen = (r:number, c:number): void => {
-        let value = _getValue[r][c]();
-        value += value>=100 ? -100 : 100;
-        changeTableValue(r,c,value);
-    }
-
-    const [auto, setAuto] = useState(false);
-
-    useEffect(() => {
-        setTimeout(()=>{
-            if(auto){
-                changeIndex(1); 
-            }
-        }, 1000/fps);
-    });
-
-    const [fps, setFps] = useState<number>(1);
-    function getFpsValue(str: string): number{
-        setAuto(false);
-        let fps = Number(str);
-        return getNumberValue(fps,fps_min,fps_max);
-    }
-
-    function getNumberValue(n: number, min: number, max: number){
-        if(Number.isNaN(n)) return 1;
-        return (n>max)?max : (n<min)?min : n;
+        USESTATE[i] = new Array(N);
+        GETVALUE[i] = new Array(N);
     }
 
     return (
         <div className='Simulator'>
-            <VscChevronLeft className='arrow' onClick={()=>{setAuto(false); changeIndex(-1);}}/>
+            <View/>
+        </div>
+    )
+} 
+
+const View = () =>{
+    const [index,setIndex] = useState<number>(INDEX);
+
+    const [autoToggle, setAutoToggle] = useState<boolean>(false);
+
+    AUTO_FUNC = ():void => {
+        if(!AUTO_TOGGLE || INDEX+1 > HISTORY.length){
+            stopAuto();
+            return;
+        } 
+        let pos: number[] = Decrypt(HISTORY[INDEX]);
+        changeValue(pos[0], pos[1]);
+        INDEX++;
+        setIndex(INDEX);
+    }
+
+    const startAuto = ():void => {
+        if(index==HISTORY.length) setIndex(0);
+        AUTO = setInterval(()=>AUTO_FUNC(), 1000/FPS);
+        AUTO_TOGGLE = true;
+        setAutoToggle(true);
+    }
+    const stopAuto = ():void => {
+        clearInterval(AUTO);
+        AUTO_TOGGLE = false;
+        setAutoToggle(false);
+    }
+
+    const [fps, setFps] = useState<number>(FPS);
+
+    const setFpsValue = (str: string): void => {
+        FPS = getFpsValue(str);
+        setFps(FPS);
+        if(AUTO_TOGGLE){
+            stopAuto();
+            startAuto();
+        }
+    }
+    const getFpsValue = (str: string): number => {
+        let fps = Number(str);
+        return getNumberValue(fps,fps_min,fps_max);
+    }
+    const getNumberValue = (n: number, min: number, max: number) => {
+        if(Number.isNaN(n)) return 1;
+        return (n>max)?max : (n<min)?min : n;
+    }
+    
+    return(
+        <div className='view'>
+            <VscChevronLeft className='arrow' onClick={()=>{stopAuto(); changeIndex(-1); setIndex(INDEX);}}/>
             <Table N={N}/>
-            <VscChevronRight className='arrow' onClick={()=>{setAuto(false); changeIndex(1);}}/>
+            <VscChevronRight className='arrow' onClick={()=>{stopAuto(); changeIndex(1); setIndex(INDEX);}}/>
             {  
-                auto
-                ? (<VscDebugPause className='start_pause' onClick={()=>{setAuto(false);}}/>)
-                : (<VscDebugStart className='start_pause' onClick={()=>{setAuto(true); if(index==History.length) setIndex(0);}}/>)
+                autoToggle
+                ? (<VscDebugPause className='start_pause' onClick={()=>stopAuto()}/>)
+                : (<VscDebugStart className='start_pause' onClick={()=>startAuto()}/>)
             }
-            {index}/{History.length}
+            {index}/{HISTORY.length}
             <div className="FPS">
                 <div className="row">
                 <p>초당 프레임 수 : </p>
-                <input type="text" value={fps} onFocus={e => e.target.select()} onChange={e => setFps(getFpsValue(e.target.value))}></input>
+                <input type="text" value={fps} onFocus={e => e.target.select()} onChange={e => setFpsValue(e.target.value)}></input>
                 </div>
-                <input type="range" value={fps} min={fps_min} max={fps_max} step={1} onChange={e => setFps(getFpsValue(e.target.value))}></input>
+                <input type="range" value={fps} min={fps_min} max={fps_max} step={1} onChange={e => setFpsValue(e.target.value)}></input>
             </div>
         </div>
-    )
+    );
 }
 
 const Table = ({N}: {N:number}) => {
@@ -130,11 +160,11 @@ const Row = ({N, row}: {N: number, row:number}) => {
 }
 const Column = ({row, column}: {row:number, column:number}) => {
     const [value, setValue] = useState<number>(0);
-    _useState[row][column] = setValue;
+    USESTATE[row][column] = setValue;
     const getValue = ():number => {
         return value;
     }
-    _getValue[row][column] = getValue;
+    GETVALUE[row][column] = getValue;
 
     const style: string = ((row+column)%2==0) ? "black" : "white";
     const element: JSX.Element = (value>=100) ? (<img src={require("../../img/LightQueen.webp")}></img>) : (<div></div>);
@@ -146,4 +176,4 @@ const Column = ({row, column}: {row:number, column:number}) => {
     )
 }
 
-export default N_Queen_Simulator;
+export default Simulator;
