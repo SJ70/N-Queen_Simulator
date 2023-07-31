ae86bad704b7005955035c658ae75b68221156cb
SJ70 <seojae70@gmail.com>
2023-07-31 16:22:42 +0900

    Refactor: 핸들러 사용 및 불필요한 validate 제거

--- src/simulator/N_Queen/Initializer.tsx
+++ src/simulator/N_Queen/Initializer.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {ChangeEvent, FocusEvent, useState} from 'react';
 import { Link } from 'react-router-dom';
 import './Initializer.css';
 
@@ -8,12 +8,19 @@ const N_MAX: number = 14;
 const Initializer = () => {
   const [n, setN] = useState<number>(8);
 
+  const handleInputFoucs = (e: FocusEvent<HTMLInputElement>) =>{
+    e.target.select();
+  }
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) =>{
+    setN(+e.target.value);
+  }
+
   return (
     <div className="Init">
       <div className="N">
         <div className="row">
           <p>N = </p>
-          <input type="text" value={n} onFocus={e => e.target.select()} onChange={e => setN(getNValue(e.target.value))}></input>
+          <input type="text" value={n} onFocus={handleInputFoucs} onChange={handleInputChange}></input>
         </div>
         <input type="range" value={n} min={N_MIN} max={N_MAX} step={1} onChange={e => setN(Number(e.target.value))}></input>
       </div>
@@ -23,16 +30,6 @@ const Initializer = () => {
       <p className="desc">성능에 따라 시간이 소요될 수 있습니다.</p>
     </div>
   );
-}
-
-function getNValue(str: string): number{
-  let n = Number(str);
-  return getNumberValue(n,n_min,n_max);
-}
-
-function getNumberValue(n: number, min: number, max: number){
-  if(Number.isNaN(n)) return 1;
-  return (n>max)?max : (n<min)?min : n;
 }
 
 export default Initializer;
