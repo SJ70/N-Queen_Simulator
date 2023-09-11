3e4edbe5827e82d2c3a92db6d3e4a6b3cecfa771
SJ70 <seojae70@gmail.com>
2023-09-11 15:20:09 +0900

    Remove: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from './components/Header'
 import Menu from './pages/Menu';
-import Problem from './class/Problem';
 import Problems from './class/Problems';
 import Simulator from './pages/Simulator';
 
@@ -13,7 +10,6 @@ const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header/>
         <Routes>
           <Route path="/" element={<Menu Probs={P}/>}></Route>
           <Route path="/:id/*" element={<Simulator Probs={P}/>}></Route>
