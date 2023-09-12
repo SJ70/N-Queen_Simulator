90617b7bf5e6e30057a3d49ad8a469ebf8447e72
SJ70 <seojae70@gmail.com>
2023-09-12 11:18:20 +0900

    Fix: github 배포 라우팅 오류 수정: BrowserRouter -> HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Simulator from './pages/Simulator';
 
 const App = () => {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/*" element={<Simulator/>}></Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
